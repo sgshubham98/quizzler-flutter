695f02256a80a9a6a4178978be3626171ef9d1bd
Shubham Goswami <sgshubham98@gmail.com>
2020-01-09 19:25:59 +0530

    answer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'questions.dart';
+
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -27,10 +29,10 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  List<Question> questionBank = [
+    Question(q:'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q:'Approximately one quarter of human bones are in the feet.', a:true),
+    Question(q:'A slug\'s blood is green.', a:true)
   ];
 
   int questionNumber = 0;
@@ -47,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -71,6 +73,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+
+                bool correctAnswer = questionBank[questionNumber].answerText;
+
+                if(correctAnswer == true)
+                  print('Hooray!!');
+                else
+                  print('Boo!');
+
                 setState(() {
                   scoreKeeper.add(
                     Icon(Icons.check, color: Colors.green),
@@ -94,6 +104,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+
+                bool correctAnswer = questionBank[questionNumber].answerText;
+
+                if(correctAnswer == false)
+                  print('Hooray!!');
+                else
+                  print('Boo!');
+
                 setState(() {
                   scoreKeeper.add(
                     Icon(Icons.close, color: Colors.red),
---
+++ lib/questions.dart
@@ -0,0 +1,9 @@
+class Question{
+  String questionText;
+  bool answerText;
+
+  Question({q: String, a: bool}){
+    questionText = q;
+    answerText = a;
+  }
+}
